71cd8af8842c60b1bb576a54b475d5f919f6bd99
Sanjay Khadka <khadka.sk7@gmail.com>
2024-09-25 01:49:47 +0545

    feat : body-parser

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require('body-parser')
 
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
-    console.log("Middleware 1")
-    next()
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/add-form', (req, res, next) => {
+    res.send("<form action='/submit-form' method='POST'><input type='text' name='name'><button type='submit'>submit</button></form>")
 })
 
-app.use((req, res, next) => {
-    console.log("Conflict in two res.send function so comment this one")
-    // res.send("heero");
-    next()
+
+// app.use('/submit-form', (req, res, next) => {
+//     console.log("body",req.body) //gives undefined without  without parserr
+//     res.redirect('/')
+// })
+
+app.post('/submit-form', (req, res, next) => {
+    console.log("body",req.body) //gives undefined without  without parserr
+    res.redirect('/')
 })
 
-app.use('/test-route',(req,res,next)=>{
-    res.send("<h1>Hello test route</h1>")
+app.get('/test-route',(req,res,next)=>{
+    res.send("<h2>Testing Get Method</h2>")
 })
 
-app.use('/test-route-other',(req,res,next)=>{
-    res.send("<h1>Hello test route other </h1>")
+
+
+
+
+
+
+app.use('/', (req, res, next) => {
+    res.send("<h1>Hello</h1><p>Parse Request</p>")
 })
+
+
 
 // const server = http.createServer(app);
 // server.listen(port);
-
 app.listen(port)
